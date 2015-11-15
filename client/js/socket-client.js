8b4f71d8a2d430dc1695466f71776b71f03f6ec0
Hunter Lightman <git.huntrr@gmail.com>
2015-11-15 15:53:21 -0500

    Finished basic server implementation

--- client/js/socket-client.js
+++ client/js/socket-client.js
@@ -1,8 +1,122 @@
 'use strict';
 var socket = io('http://localhost:3000');
 
-socket.on('status', function (data) {
-  if(data.type === 'hello');
-  console.log('Got HELLO');
-  socket.emit('status', {type: 'hello'});
+// stores user in channel. It's a dictionary with id as key
+// User objects have these fields
+//   - id     (user id)
+//   - name   (user name)
+//   - string (string representation)
+var users = {};
+
+// stores the client user id
+// hence users[me] or users.me gives the client user
+var me = socket.id;
+
+// ---------------------
+//    SOCKET HANDLERS
+// ---------------------
+
+/**
+ * Handles STATE events.
+ * These are fired when the client first connects,
+ * the data object contains:
+ *   - users (list of user objects currently connected)
+ *   - user  (the id of the current client)
+ */
+socket.on('STATE', function (data) {
+  users = data.users;
+  me = data.user;
+  console.log(':STATE - Users in channel: ' + getUserList());
 });
+
+/**
+ * Handles JOINED events.
+ * When a new user joins.
+ * Data object contains:
+ *   - user (the user that just joined)
+ */
+socket.on('JOINED', function (data) {
+  var user = data.user;
+  users[user.id] = user;
+  console.log(':JOINED - ' + user.string);
+});
+
+/**
+ * Handles LEFT events.
+ * Deletes users who leave.
+ * Data object:
+ *   - user (the user that left)
+ */
+socket.on('LEFT', function (data) {
+  var user = data.user;
+  console.log(':LEFT - ' + user.string);
+  delete users[user.id];
+});
+
+/**
+ * Handles MESG events.
+ * For messages to the client.
+ * Data object:
+ *   - from    (the NAME of the user the message is from)
+ *   - message (the message)
+ */
+socket.on('MESG', function (data) {
+  console.log(':MSG - <' + data.from + '> ' + data.message);
+});
+
+/**
+ * Handles NAME events.
+ * Updates a users name.
+ * Data object:
+ *   - user (the updated user object)
+ */
+socket.on('NAME', function (data) {
+  var user = data.user;
+  var old = users[user.id];
+  users[user.id] = user;
+  
+  console.log(':NAME - <' + old.string + '> changed to <' + user.name + '>');
+});
+
+/**
+ * Handles ERROR events.
+ * Data object:
+ *   - message
+ */
+socket.on('ERROR', function (data) {
+  console.log(':ERROR - ' + data.message);
+});
+
+
+
+// ---------------
+//     HELPERS
+// ---------------
+
+/**
+ * Showcases functional Javascript (_.fold) and ternary operators
+ * to get a list of the users currently chatting
+ */
+function getUserList() {
+  return _.reduce(users,
+                  function (rest, user) { 
+                    return (rest ? rest + ', ' : '') + user.name;
+                  },
+                  ''
+                 );
+}
+
+/**
+ * Sends a MESG to the server
+ */
+function sendMessage(message) {
+  socket.emit('MESG', {message: message});
+}
+
+/**
+ * Sends a NAME message to the server
+ * changing this users name
+ */
+function setName(newName) {
+  socket.emit('NAME', {newName: newName});
+}
--- server/server.js
+++ server/server.js
@@ -1,26 +1,116 @@
 'use strict'; // puts us in strict mode (js in hard mode)
 
-// grabs our dependencies
-var http = require('http');
+// gets imports
+let Moniker = require('moniker');
+let _ = require('underscore');
+let User = require('./user');
 
 
-// set up the http server
-var server = http.createServer()
-server.listen(3000); // run on localhost
-console.log('HTTP server started on 3000');
+// sets up the server
+let Server = require('socket.io');
+let io = Server(3000); //construct a server on port 3000
+console.log('SocketIO listening on port 3000');
 
 
-// bind socket.io to the http server
-var io = require('socket.io')(server);
 
+// stores the users
+// it's a dictionary where the userID is the key
+let users = {};
 
-// test stuff
-io.on('connection', function (socket) {
-  console.log('Got connection');
-  socket.emit('status', {type: 'hello'});
-  socket.on('status', function (data) {
-    if(data.type == 'hello') {
-      console.log('Got HELLO!');
+// SOCKET HANDLER FUNCTIONS
+io.on('connection', (socket) => {
+  // on 'connection' we need to set some stuff up
+  let name = getUniqueName(); // get a unique name
+  let user = User(socket, name); // create a User (showcases factories)
+  users[socket.id] = user; // adds user to dictionary
+  console.log(`:CONNECTION - ${user.toString()})`);
+
+  // emit the current state to the new user
+  socket.emit('STATE', {
+    users: _.mapObject(users, (user) => user.toObj()), //_ for functional js
+    user: user.getId()
+  });
+
+  // emit the new JOIN for all the other users
+  socket.broadcast.emit('JOINED', {user: users[socket.id].toObj()});
+
+  // -----------------
+  //  SOCKET HANDLERS
+  // -----------------
+  /**
+   * Handles MESG
+   * When a client tries to speak
+   * Data object contains:
+   *   - message
+   */
+  socket.on('MESG', (data) => {
+    let user = users[socket.id];
+    console.log(`:MESG - <${user.getName()}> ${data.message}`);
+    
+    let message = {
+      from: user.getName(),
+      message: data.message
+    };
+
+    io.emit('MESG', message); // broadcast the message everywhere
+  });
+
+  /**
+   * Handles NAME
+   * When a client tries to change their name
+   * Data object contains:
+   *   - newName
+   */
+  socket.on('NAME', (data) => {
+    let user = users[socket.id];
+    console.log(`:NAME - <${user.getName()}> wants to change name to`
+                + ` <${data.newName}>`);
+
+    if(isUniqueName(data.newName)) {
+      // success!
+      console.log(
+        `:NAME - <${user.getName()}> changed name to <${data.newName}>`);
+      user.setName(data.newName);
+      io.emit('NAME', {user: user.toObj()});
+    } else {
+      // failure :(
+      console.log(':ERROR - NON_UNIQUE_NAME');
+      socket.emit('ERROR', {message: 'NON_UNIQUE_NAME'});
     }
   });
+
+
+  /** Handles diconnect */
+  socket.on('disconnect', () => {
+    let user = users[socket.id];
+    console.log(`:LEFT - ${user.toString()})`);
+    socket.broadcast.emit('LEFT', {user: user.toObj()});
+    delete users[socket.id];
+  });
 });
+
+
+
+// HELPER FUNCTIONS
+/**
+ * Sees if a name is unique
+ * @param name The name to check
+ * @return boolean true if the name is unique
+ */
+function isUniqueName(name) {
+  let names = _.mapObject(users, (user) => user.getName().toLowerCase());
+  return !_.contains(names, name.toLowerCase());
+}
+
+/**
+ * Gets a unique name using Moniker (showcases basic npm modules)
+ * @return String a unique name
+ */
+function getUniqueName() {
+  let name = Moniker.choose();
+  while(!isUniqueName(name)) {
+    name = Moniker.choose();
+  }
+
+  return name;
+}
